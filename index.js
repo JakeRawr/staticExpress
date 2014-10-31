f7cd959feff6ad7ac3545a1e84496c3ee9bd9493
Jake Yang <tsunghan614@gmail.com>
2014-10-31 09:26:51 -0700

    static changes

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ var express = require('express');
 var app = express();
 var http = require('http');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/nextLink', function (req, res) {
   res.send('Link Connected');
-});
-
-app.get('/', function (req, res) {
-  console.log('Home Connected');
-
-  res.sendFile('/Users/Tsunghan/devjs/staticExpress/index.html', function (err) {
-    if (err) {
-      console.log(err);
-      res.status(err.status).end();
-    }
-    else {
-      console.log('Sent: Home');
-    }
-  });
 });
 
 var port = process.env.PORT || 3000;
